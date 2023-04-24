20a3db3427ec84199cb8a00c2e9f84217b6a15b3
Rishab Maheshwari <rishabmahesh@umass.edu>
2023-04-23 22:16:07 -0400

    added code to filter by size

--- src/pages/tab_pages/product_list.tsx
+++ src/pages/tab_pages/product_list.tsx
@@ -15,8 +15,6 @@ import {
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import RNBottomActionSheet from 'react-native-bottom-action-sheet';
-
 import {HomeStackScreenProps} from '../../type';
 
 import {COLOR} from '../../../assets/setting';
@@ -37,6 +35,7 @@ function ProductListPage({
 }: HomeStackScreenProps<'ProductListPage'>): JSX.Element {
   // const [modalVisible, setModalVisible] = useState(false);
   let order = undefined;
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
@@ -59,9 +58,10 @@ function ProductListPage({
           items={[
             {label: 'Price: Low to High', value: 1},
             {label: 'Price: High to Low', value: -1},
+            {label: 'Size: XS', value: 'XSmall'},
             {label: 'Size: S', value: 'Small'},
-            {label: 'Size: L', value: 'Large'},
             {label: 'Size: M', value: 'Medium'},
+            {label: 'Size: L', value: 'Large'},
             {label: 'Size: XL', value: 'XLarge'},
           ]}
         />
@@ -73,11 +73,187 @@ function ProductListPage({
   // const [products, setProducts] = useState([]);
   const [lst, setLst] = useState([[], []]);
   const [orderState, setOrder] = useState(undefined);
+  const [filteredList, setFilteredList] = useState([]);
 
   const categrory = route.params.category;
   const fetchData = async () => {
     const id = await AsyncStorage.getItem('user_id');
-    let _products = await GetProductList(id, categrory);
+    let _products = [
+      {
+        id: 1,
+        category: 0,
+        size: 'S',
+        price: 15,
+        brand: 'H&M',
+        usage: 'Almost new',
+        image: 'cover image2.jpg',
+        height: 408,
+        width: 612,
+        seller: 1,
+        meeting: 0,
+      },
+      {
+        id: 2,
+        category: 0,
+        size: 'M',
+        price: 10,
+        brand: 'Uniqlo',
+        usage: 'New',
+        image: 'cover image2.jpg',
+        height: 664,
+        width: 1689,
+        seller: 2,
+        meeting: 0,
+      },
+      {
+        id: 3,
+        category: 0,
+        size: 'L',
+        price: 20,
+        brand: 'Zara',
+        usage: 'Almost new',
+        image: 'cover image2.jpg',
+        height: 408,
+        width: 612,
+        seller: 5,
+        meeting: 1,
+      },
+      {
+        id: 4,
+        category: 0,
+        size: 'XS',
+        price: 13,
+        brand: 'H&M',
+        usage: 'New',
+        image: 'cover image2.jpg',
+        height: 664,
+        width: 1689,
+        seller: 3,
+        meeting: 1,
+      },
+      {
+        id: 5,
+        category: 1,
+        size: '10',
+        price: 20,
+        brand: 'Zara',
+        usage: 'Almost new',
+        image: 'cover image2.jpg',
+        height: 408,
+        width: 612,
+        seller: 4,
+        meeting: 0,
+      },
+      {
+        id: 6,
+        category: 1,
+        size: '12',
+        price: 10,
+        brand: 'H&M',
+        usage: 'Almost new',
+        image: 'cover image2.jpg',
+        height: 664,
+        width: 1689,
+        seller: 3,
+        meeting: 0,
+      },
+      {
+        id: 7,
+        category: 1,
+        size: '8',
+        price: 15,
+        brand: 'Uniqlo',
+        usage: 'Almost new',
+        image: 'cover image2.jpg',
+        height: 408,
+        width: 612,
+        seller: 2,
+        meeting: 1,
+      },
+      {
+        id: 8,
+        category: 1,
+        size: '9',
+        price: 23,
+        brand: 'Uniqlo',
+        usage: 'New',
+        image: 'cover image2.jpg',
+        height: 664,
+        width: 1689,
+        seller: 2,
+        meeting: 1,
+      },
+      {
+        id: 9,
+        category: 2,
+        size: 'S',
+        price: 30,
+        brand: 'EMS',
+        usage: 'Almost new',
+        image: 'cover image2.jpg',
+        height: 408,
+        width: 612,
+        seller: 1,
+        meeting: 0,
+      },
+      {
+        id: 10,
+        category: 2,
+        size: 'L',
+        price: 50,
+        brand: 'Columbia',
+        usage: 'Almost new',
+        image: 'cover image2.jpg',
+        height: 664,
+        width: 1689,
+        seller: 1,
+        meeting: 0,
+      },
+      {
+        id: 11,
+        category: 2,
+        size: 'XL',
+        price: 25,
+        brand: 'The North Face',
+        usage: 'New',
+        image: 'cover image2.jpg',
+        height: 408,
+        width: 612,
+        seller: 3,
+        meeting: 1,
+      },
+      {
+        id: 12,
+        category: 2,
+        size: 'M',
+        price: 40,
+        brand: 'Timberland',
+        usage: 'Almost new',
+        image: 'cover image2.jpg',
+        height: 664,
+        width: 1689,
+        seller: 2,
+        meeting: 1,
+      },
+    ];
+    // let _products = await GetProductList(id, categrory);
+    if (orderState === 'XSmall') {
+      _products = _products.filter(product => product.size === 'XS');
+      setFilteredList(_products);
+    } else if (orderState === 'Small') {
+      _products = _products.filter(product => product.size === 'S');
+      setFilteredList(_products);
+    } else if (orderState === 'Medium') {
+      _products = _products.filter(product => product.size === 'M');
+      setFilteredList(_products);
+    } else if (orderState === 'Large') {
+      _products = _products.filter(product => product.size === 'L');
+      setFilteredList(_products);
+    } else if (orderState === 'XLarge') {
+      _products = _products.filter(product => product.size === 'XL');
+      setFilteredList(_products);
+    }
+
     if (orderState !== undefined) {
       _products.sort(
         (a, b) => orderState * (Number(a.price) - Number(b.price)),
@@ -112,7 +288,35 @@ function ProductListPage({
   return (
     <ScrollView>
       <View style={styles.container}>
-        {load &&
+        {filteredList.length !== 0 &&
+          load &&
+          filteredList.map(item => {
+            return (
+              <TouchableOpacity
+                style={styles.product}
+                activeOpacity={0.5}
+                onPress={() => {
+                  navigation.push('ProductDescriptionPage', {
+                    category: item.category,
+                    product: item._id,
+                  });
+                }}>
+                <AutoHeightImage
+                  source={{uri: GetImage(item.images[0].name)}}
+                  width={200}
+                />
+                <Text
+                  style={styles.price}
+                  adjustsFontSizeToFit={true}
+                  numberOfLines={1}>
+                  ${item.price}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+
+        {filteredList.length === 0 &&
+          load &&
           lst.map(items => {
             return (
               <View style={styles.list}>
